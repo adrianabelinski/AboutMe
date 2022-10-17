3fcd8cfe579087e6b6bbae5a143c913a52a58d9c
Adriana Belinski <adrianabmpls@gmail.com>
2022-10-16 20:11:21 -0500

    Initial commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Adriana Belinski on 10/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
