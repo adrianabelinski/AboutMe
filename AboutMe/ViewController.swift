062ea14e542d78381c42a00aaea48a012096d8ff
Adriana Belinski <adrianabmpls@gmail.com>
2022-10-20 18:06:45 -0500

    Added 3 colored blocks and a button.

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -19,13 +19,6 @@ class ViewController: UIViewController {
     view.layer.maskedCorners  =  [.layerMaxXMinYCorner, .layerMinXMinYCorner]
     return view
   }()
-  
-  let linksColoredBlock: UIView = {
-    let view = UIView()
-    view.translatesAutoresizingMaskIntoConstraints = false
-    view.backgroundColor = UIColor(named: "Sunny")
-    return view
-  }()
 
   let myPhotoView: UIImageView = {
     
@@ -43,13 +36,43 @@ class ViewController: UIViewController {
     label.translatesAutoresizingMaskIntoConstraints = false
     label.text = "Adriana Belinski"
     label.textAlignment = .center
-   label.font = UIFont(name: "SF-Compact", size: 24)
+    label.font = UIFont(name: "SF-Compact", size: 24)
     return label
+  }()
+  
+  let linksColoredBlock: UIView = {
+    let view = UIView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.backgroundColor = UIColor(named: "Sunny")
+    return view
+  }()
+  
+  let likesSection: UIView = {
+    let view = UIView()
+    view.backgroundColor = UIColor(named: "Sunny")
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
+  let introToCodingSection: UIView = {
+    let view = UIView()
+    view.backgroundColor = UIColor(named: "Sunny")
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
+  let sayHello: UIButton = {
+    let button = UIButton()
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.backgroundColor = UIColor(named: "Orange")
+    button.layer.cornerRadius = 20
+    button.setTitle("Say Hello! 👋", for: .normal)
+    return button
   }()
   
   
   // MARK: - Lifecycle
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -69,6 +92,9 @@ class ViewController: UIViewController {
     setUpMyPhoto()
     setUpName()
     setUpLinksColoredBlock()
+    setLikesSection()
+    setIntroToCodingSection()
+    setUpSayHello()
   }
   
   func setUpColoredBlock() {
@@ -76,8 +102,8 @@ class ViewController: UIViewController {
     
     NSLayoutConstraint.activate([
       coloredBlock.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
-      coloredBlock.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-      coloredBlock.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+      coloredBlock.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
+      coloredBlock.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
       coloredBlock.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3)
     ])
   }
@@ -87,8 +113,8 @@ class ViewController: UIViewController {
     
     NSLayoutConstraint.activate([
       myPhotoView.topAnchor.constraint(equalTo: coloredBlock.topAnchor, constant: 20),
-      myPhotoView.leadingAnchor.constraint(equalTo: coloredBlock.leadingAnchor, constant: 63),
-      myPhotoView.trailingAnchor.constraint(equalTo: coloredBlock.trailingAnchor, constant: -63),
+      myPhotoView.leadingAnchor.constraint(equalTo: coloredBlock.leadingAnchor, constant: 43),
+      myPhotoView.trailingAnchor.constraint(equalTo: coloredBlock.trailingAnchor, constant: -43),
       myPhotoView.heightAnchor.constraint(equalToConstant: 170),
     ])
   }
@@ -109,10 +135,45 @@ class ViewController: UIViewController {
     
     NSLayoutConstraint.activate([
       linksColoredBlock.topAnchor.constraint(equalTo: coloredBlock.bottomAnchor, constant: 20),
-      linksColoredBlock.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-      linksColoredBlock.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: -20),
+      linksColoredBlock.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
+      linksColoredBlock.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
       linksColoredBlock.heightAnchor.constraint(equalToConstant: 100),
     ])
   }
+  
+  
+  func setLikesSection() {
+    view.addSubview(likesSection)
+    
+    NSLayoutConstraint.activate([
+      likesSection.topAnchor.constraint(equalTo: linksColoredBlock.bottomAnchor, constant: 20),
+      likesSection.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
+      likesSection.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
+      likesSection.heightAnchor.constraint(equalToConstant: 75),
+    ])
+  }
+  
+  func setIntroToCodingSection() {
+    view.addSubview(introToCodingSection)
+    
+    NSLayoutConstraint.activate([
+      introToCodingSection.topAnchor.constraint(equalTo: likesSection.bottomAnchor, constant: 20),
+      introToCodingSection.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
+      introToCodingSection.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
+      introToCodingSection.heightAnchor.constraint(equalToConstant: 100),
+    ])
+  }
+  
+  func setUpSayHello() {
+    view.addSubview(sayHello)
+    
+    NSLayoutConstraint.activate([
+      sayHello.topAnchor.constraint(equalTo: introToCodingSection.bottomAnchor, constant: 30),
+      sayHello.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 65),
+      sayHello.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -65),
+      sayHello.heightAnchor.constraint(equalToConstant: 75),
+    ])
+  }
+  
 }
 
